485a47fef86bf81e5be7322f9653334edc4a68d4
dlford <dan-github2@dlford.co>
2020-12-07 07:01:47 -0700

    Refactor fan-controller.sh
    Reduced log spam by only logging when fan state is changed
    Add options for controlling polling time based on temperature
      Default is 15 seconds when under fan start temp, and 60 seconds when
      over fan start temp.

--- fan-controller.sh
+++ fan-controller.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
 
-maxTemp=55 # Fan on
+startTemp=65 # Fan on temp
 fanGPIO=18 # GPIO Pin
+lowPollingTime=15 # Polling seconds when under start temp
+highPollingTime=60 # Polling seconds when over start temp
 
-echo "$fanGPIO" > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio$fanGPIO/direction
+echo "$fanGPIO" > /sys/class/gpio/export > /dev/null 2>&1
+echo "out" > /sys/class/gpio/gpio$fanGPIO/direction > /dev/null 2>&1
 
-while [ "1" = "1" ]
-do
-temp=`vcgencmd measure_temp | cut -c6,7`
-echo "Temperature: $temp"
-if [ $temp -ge $maxTemp ]
-then
-  echo "Fan started: "$(date)
-  echo "1" > /sys/class/gpio/gpio$fanGPIO/value
-elif [ $temp -lt $maxTemp ]
-then
-  echo "Fan stopped: "$(date)
-  echo "0" > /sys/class/gpio/gpio$fanGPIO/value
-fi
-sleep 30
+while [ "1" = "1" ]; do
+	temp=$(vcgencmd measure_temp | cut -c6,7)
+	fanStatus="$(cat /sys/class/gpio/gpio$fanGPIO/value)"
+	if [ $temp -ge $startTemp ]; then
+		if [ $fanStatus -eq 0 ]; then
+			echo "1" > /sys/class/gpio/gpio$fanGPIO/value
+			echo "Temperature: $temp; Fan started"
+		fi
+		sleep $highPollingTime
+	elif [ $temp -lt $startTemp ]; then
+		if [ $fanStatus -eq 1 ]; then
+			echo "0" > /sys/class/gpio/gpio$fanGPIO/value
+			echo "Temperature: $temp; Fan stopped"
+		fi
+		sleep $lowPollingTime
+	fi
 done
